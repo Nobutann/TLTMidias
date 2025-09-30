e82372db4dfbf70f407517390cfa67748f7b4332
Nobutann <luizgonzagafilho201365@gmail.com>
2025-09-30 11:55:51 -0300

    criando models de artigo, categoria e subcategoria

--- article/models.py
+++ article/models.py
@@ -2,4 +2,28 @@ from django.db import models
 
 class Categories(models.Model):
     title = models.CharField(max_length=100)
-    slug = models.SlugField()
+    slug = models.SlugField(unique=True)
+    
+    def __str__(self):
+        return self.title
+    
+class Article(models.Model):
+    title = models.CharField(max_length=200)
+    content = models.TextField()
+    category = models.ForeignKey(Categories, on_delete=models.SET_NULL, null=True, related_name='articles')
+    published_date = models.DateTimeField(auto_now_add=True)
+    author = models.CharField(max_length=200)
+
+    def __str__(self):
+        return f"{self.title}({self.author})"
+    
+class Subcategory(models.Model):
+    category = models.ForeignKey(Categories, on_delete=models.CASCADE)
+    title = models.CharField(max_length=100)
+    slug = models.SlugField(unique=True)
+
+    def __str__(self):
+        return f"{self.title}({self.category})"
+
+class Comments(models.Model):
+    pass
