e20de0b27959741f30cd508a6f60dca580212fb5
Nobutann <luizgonzagafilho201365@gmail.com>
2025-10-18 16:07:12 -0300

    criada view da homepage

--- article/views.py
+++ article/views.py
@@ -1,67 +1,23 @@
 from django.shortcuts import render, get_object_or_404, redirect
-from .models import Article, Category, Subcategory 
+from .models import Article, Category
 from .forms import ArticleForms
+from django.core.paginator import Paginator
 
-def home(request):
-    categories = Category.objects.all().order_by('title')
+def homepage(request):
+    articles_list = Article.objects.all().order_by('-published_date')
+    categories = Category.objects.all()
+
+    paginator = Paginator(articles_list, 10)
+    page_number = request.GET.get('page')
     
-    subcategories = Subcategory.objects.select_related('category').all()
-    
-    menu_data = {}
-    
-    
-    for category in categories:
-        menu_data[category.title] = {
-            'slug': category.slug,
-            'subcategories': []
-        }
-        
-    
-    for subcategory in subcategories:
-        category_title = subcategory.category.title
-        if category_title in menu_data:
-            menu_data[category_title]['subcategories'].append(subcategory) 
+    articles = paginator.get_page(page_number)
 
-    context = {'menu': menu_data}
-    return render(request, 'main/index.html', context)
+    context = {
+        'categories': categories,
+        'articles': articles,
+    }
 
-
-def category_articles(request, slug):
-    category = get_object_or_404(Category, slug=slug)
-    
-    articles = Article.objects.filter(category=category).order_by('-published_date') 
-    context = {'title': category.title, 'articles': articles}
-    return render(request, 'article/list_page.html', context)
-
-
-
-def subcategory_articles(request, slug):
-    subcategory = get_object_or_404(Subcategory, slug=slug)
-    
-    articles = Article.objects.filter(subcategory=subcategory).order_by('-published_date')
-    context = {'title': subcategory.title, 'articles': articles}
-    return render(request, 'main/index.html', context)
-
-def article_detail(request, slug):
-    """Exibe o artigo pelo slug ou mostra uma página de erro se não for encontrado."""
-    try:
-        article = Article.objects.get(slug=slug)
-        context = {'article': article}
-        return render(request, 'article/detail.html', context)
-
-    except Article.DoesNotExist:
-        referer = request.META.get('HTTP_REFERER', '/')
-        return render(request, 'article/error.html', {
-            'message': 'Artigo não encontrado',
-            'referer': referer
-        })
-
-    except Exception:
-        referer = request.META.get('HTTP_REFERER', '/')
-        return render(request, 'article/error.html', {
-            'message': 'Erro ao carregar o artigo',
-            'referer': referer
-        })
+    return render(request, 'article/index.html', context)
 
 def publish_article(request):
     if request.method == 'POST':
--- main/urls.py
+++
@@ -1,7 +0,0 @@
-from django.urls import path
-from . import views  # Importa as views do app atual
-
-
-urlpatterns = [
-    path('', views.home, name='home'),
-]
