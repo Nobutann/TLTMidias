d61b0530d89c3ae66291f26b3cf35bfcd6474eaa
Nobutann <luizgonzagafilho201365@gmail.com>
2025-11-05 22:13:31 -0300

    corrigindo javascript

--- static/article/js/scripts.js
+++ static/article/js/scripts.js
@@ -18,3 +18,35 @@ function toggleResponseForm(commentId) {
         form.style.display = 'none';
     }
 }
+
+document.addEventListener('DOMContentLoaded', function() {
+    const btn = document.getElementById('showNotifications');
+    const box = document.getElementById('notificationList');
+    const list = document.getElementById('notifItems');
+
+    if (!btn) return;
+
+    btn.addEventListener('click', async () => {
+    if (box.style.display === 'none') {
+        const resp = await fetch('/notifications/inbox/', { credentials: 'include' });
+        if (!resp.ok) {
+            alert('Erro ao buscar notificações');
+            return;
+        }
+        const data = await resp.json();
+        list.innerHTML = '';
+        if (data.items.length === 0) {
+            list.innerHTML = '<li>Sem notificações.</li>';
+        } else {
+            data.items.forEach(n => {
+                const li = document.createElement('li');
+                li.innerHTML = `<a href="${n.url}" target="_blank">${n.title}</a><br><small>${n.category}</small>`;
+                list.appendChild(li);
+            });
+        }
+        box.style.display = 'block';
+        } else {
+            box.style.display = 'none';
+        }
+      });
+    });
