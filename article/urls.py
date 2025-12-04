26b58ec2cf05ea42bfe442d86db57dbd363264ae
Luiz Gonzaga <luizgonzagafilho201365@gmail.com>
2025-12-04 09:58:38 -0300

    feito página acervo

--- article/urls.py
+++ article/urls.py
@@ -7,4 +7,5 @@ urlpatterns = [
     path('', views.homepage, name='homepage'),
     path('article/<slug:slug>/', views.article_details, name='details'),
     path('categoria/<slug:category_slug>/', views.category_page, name='category_page'),
+    path('acervo/', views.acervo, name='acervo'),
 ]
